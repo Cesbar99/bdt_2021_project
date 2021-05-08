d19af18a84a0500a08794bc777296633d5084bf6
Cesbar99 <74916917+Cesbar99@users.noreply.github.com>
2021-05-08 13:47:11 +0200

    Add files via upload

---
+++ fiumi_to_azure.py
@@ -0,0 +1,121 @@
+from __future__ import absolute_import, annotations
+
+import os
+import json
+import time
+from datetime import datetime
+from typing import List, Optional
+import requests
+import sqlite3
+import pyodbc
+import textwrap
+import pyodbc
+
+from dati_fiumi import raw_rivers
+from dati_fiumi import Rivers
+from dati_fiumi import get_rivers
+from dati_fiumi import Manager_dati_storici
+from dati_fiumi import SQLAzureRivers
+
+
+os.chdir('C:/Users/Cesare/OneDrive/studio/magistrale-data science/big data tech')
+'''
+manager = SQLAzureRivers()
+#manager.create()
+
+historic_list_rivers = Manager_dati_storici.manage_dati_storici()
+#historic_repr_rivers = [Rivers.to_repr(river) for river in historic_list_rivers]
+
+#anager.save(historic_list_rivers) ### scrivere per azure
+
+###use while and time.sleep()
+
+url = 'http://dati.retecivica.bz.it/services/meteo/v1/sensors'
+new_rivers = get_rivers(url)
+list_of_rivers = [Rivers.from_repr(new_river) for new_river in new_rivers] 
+list_of_rivers = sorted(list_of_rivers, key=lambda river: river.get_id()) ### LISTA ORDINATA PER ID: ISARCO, ADIGE, TALVERA
+
+#manager.save(list_of_rivers) 
+'''
+
+
+####
+
+create_table_query = '''
+CREATE TABLE Fiumi_tentativo
+(
+    Id INT ,
+    Name NVARCHAR(128) NOT NULL,
+    Q_mean NUMERIC NOT NULL,
+    W_mean NUMERIC NOT NULL,
+    WT_mean  NUMERIC NOT NULL,
+	Timestamp DATETIME NOT NULL,
+	Stagione NVARCHAR(128) NOT NULL
+)
+'''
+
+#query = 'INSERT into Fiumi_tentativo(id, Name, Q_mean, W_mean, WT_mean,Timestamp, Stagione) VALUES (?,?,?,?,?,?,?)'
+
+
+server_name = 'server-fiumi-bdt-2021' 
+server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
+database = 'database_fiumi_bdt_2021' 
+username = 'nome_utente' 
+password = '_Password'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+connection_string = textwrap.dedent('''
+        Driver={driver};
+        Server={server};
+        Database={database};
+        Uid={username};
+        Pwd={password};
+        Encrypt=yes;
+        TrustServerCertificate=no;
+        Connection Timeout=30;
+    '''.format(
+        driver=driver,
+        server=server,
+        database=database,
+        username=username,
+        password=password 
+        ))
+
+
+cnxn: pyodbc.Connection = pyodbc.connect(connection_string)
+crsr: pyodbc.Cursor = cnxn.cursor()
+#crs.execute(create_table_query)
+#crsr.commit()
+'''
+rivers = []
+crsr.execute('SELECT * from Tabella_Adige')
+rows = crsr.fetchall()
+print(rows)
+
+for Id, Q_mean, W_mean, WT_mean,Timestamp, Stagione in rows:
+    query = 'SELECT Name FROM Tabella_nomi WHERE Id = {id}'.format(id = Id)
+    crsr.execute(query)
+    name = crsr.fetchall()[0][0]
+    print(name)
+    rivers.append(Rivers.to_repr(Rivers(Timestamp, name , Stagione, Id, Q_mean, W_mean, WT_mean)) )
+'''
+cnxn.close()
+#print(rivers[0]['TimeStamp'])
+
+
+'''
+for river in list_of_rivers:
+    crsr.execute('SET IDENTITY_INSERT Fiumi_try ON')
+    crsr.execute(query, (river.get_id(), river.name(), river.q_mean(), river.w_mean(), river.wt_mean(), river.timestamp(), river.stagione() ))
+    crsr.commit()
+
+cnxn.close()
+'''
+
+###PER USARE LA INSERT BISOGNA ESEGUIRE QUESTO COMANDO PRIMA DI OGNI OPERAZIONE:
+#crsr.execute('SET IDENTITY_INSERT Fiumi_try ON')
+'''
+x = manager.from_db_to_list('Tabella_Isarco')
+for i in x:
+    print(Rivers.to_repr(i))
+'''
---
+++ mqtt.py
@@ -0,0 +1,54 @@
+#TO RUN ON PROMPT EXECUTE THE FOLLOWING CODE WITHOUT IMPORTING TIME AND FUNCTION TIME.SLEEP
+
+#########
+import paho.mqtt.client as mqtt #import the client1
+import time
+from datetime import datetime
+
+from dati_fiumi import Rivers, SQLAzureRivers
+
+############
+'''
+def on_message(client, userdata, message):
+    #print("message received " ,str(message.payload.decode("utf-8")))
+    print('message received')
+    print(message.payload.decode())
+    dic = eval(message.payload.decode())
+    print(dic['ID'], dic['NAME'])
+    #print(dict(message.payload.decode())['Albergo'])
+########################################
+#broker_address="broker.hivemq.com"
+broker_address="mqtt.eclipseprojects.io" #"iot.eclipse.org"
+client = mqtt.Client('fiumi-storer') #client = mqtt.Client() create new instance ; client = mqtt.Client()
+client.on_message=on_message #attach function to callback
+client.connect(broker_address) #connect to broker
+#client.loop_start() #start the loop
+client.subscribe('fiumi') #client.subscribe('testtopic/#')
+#client.loop_stop()
+client.loop_forever()
+#time.sleep(10) # wait
+'''
+def on_message(client, userdata, message):
+    #print("message received " ,str(message.payload.decode("utf-8")))
+    #print('message received')
+    #print(message.payload.decode())
+    dic = eval(message.payload.decode())
+    river = Rivers.from_repr(dic)
+    #print(dic['ID'], dic['NAME'])
+    #print(dict(message.payload.decode())['Albergo'])
+    SQLAzureRivers.save(river)
+
+########################################
+
+#broker_address="broker.hivemq.com"
+broker_address="mqtt.eclipseprojects.io" #"iot.eclipse.org"
+client = mqtt.Client('fiumi-storer') #client = mqtt.Client() create new instance ; client = mqtt.Client()
+client.on_message=on_message #attach function to callback
+client.connect(broker_address) #connect to broker
+#client.loop_start() #start the loop
+client.subscribe('fiumi') #client.subscribe('testtopic/#')
+#client.loop_stop()
+client.loop_forever()
+#time.sleep(10) # wait
+
+
